3133576a622b7c089c68315ca037bb6155cbe1b8
DylanW-Dev <dylan.wanten.dev@gmail.com>
2025-03-22 14:16:44 +0100

    Task 2 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,10 +17,14 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const ISBN = req.params.isbn;
+  if (books[ISBN]) {
+      res.send(books[ISBN]); // Send book details
+  } else {
+      res.status(404).send({ message: "Book not found" });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {

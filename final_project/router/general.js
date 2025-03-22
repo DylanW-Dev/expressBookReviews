d6a98262614d8d6a4382de9c9efd623a3f5f55e6
DylanW-Dev <dylan.wanten.dev@gmail.com>
2025-03-22 15:03:09 +0100

    Task 5 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -67,8 +67,13 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const ISBN = req.params.isbn;
+  
+  if (books[ISBN]) {
+      res.send(books[ISBN].reviews); // Send book details
+  } else {
+      res.status(404).send({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
